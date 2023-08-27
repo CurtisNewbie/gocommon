5dd450b3a780a1c5f97f0aac50484950882c97f7
yongj.zhuang <yongj.zhuang@outlook.com>
2023-08-27 21:49:02 +0800

    Migrate to miso

--- common/user.go
+++ common/user.go
@@ -1,7 +1,7 @@
 package common
 
-var (
-	nilUser = User{IsNil: true}
+import (
+	"github.com/curtisnewbie/miso/core"
 )
 
 type User struct {
@@ -12,7 +12,47 @@ type User struct {
 	IsNil    bool
 }
 
+const (
+	UserIdTraceKey   = "id"
+	UserNoTraceKey   = "userno"
+	UsernameTraceKey = "username"
+	RoleNoTraceKey   = "roleno"
+)
+
+var (
+	nilUser                = User{IsNil: true}
+	builtinPropagationKeys = []string{
+		UserIdTraceKey,
+		UserNoTraceKey,
+		UsernameTraceKey,
+		RoleNoTraceKey,
+	}
+)
+
+func init() {
+	// load builtin propagation keys, so all dependents get the same behaviour
+	for _, v := range builtinPropagationKeys {
+		core.AddPropagationKey(v)
+	}
+}
+
 // Get a 'nil' User
 func NilUser() User {
 	return nilUser
+}
+
+// Get User from Rail (trace)
+func GetUser(rail core.Rail) User {
+	idv := rail.CtxValInt(UserIdTraceKey)
+	if idv <= 0 {
+		return NilUser()
+	}
+
+	return User{
+		UserId:   idv,
+		Username: rail.CtxValStr(UsernameTraceKey),
+		UserNo:   rail.CtxValStr(UserNoTraceKey),
+		RoleNo:   rail.CtxValStr(RoleNoTraceKey),
+		IsNil:    false,
+	}
 }
